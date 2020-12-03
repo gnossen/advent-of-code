5df2ba13e24ea48ee322b2fd549059b79fb843c6
Richard Belleville <j.belleville.richard@gmail.com>
2020-12-02 17:39:59 -0800

    Add day 2 part 1 solution

---
+++ 2020/2/2.py
@@ -0,0 +1,34 @@
+import re
+import sys
+from typing import Tuple
+
+RULE_REGEX = re.compile("([0-9]+)-([0-9]+) ([a-zA-Z])")
+
+def get_rule(rule_str: str) -> Tuple[int, int]:
+    match = RULE_REGEX.match(rule_str)
+    rng = (int(match.group(1)), int(match.group(2)))
+    assert rng[0] <= rng[1], f"Rule '{rule_str}' invalid."
+    c = match.group(3)
+    return rng, c
+
+def is_valid(line: str) -> bool:
+    pieces = line.split(":")
+    assert len(pieces) == 2
+    rule_str = pieces[0].strip()
+    password = pieces[1].strip()
+    (low, high), c = get_rule(rule_str)
+    count = sum(1 for p in password if p == c)
+    return low <= count <= high
+
+if len(sys.argv) != 2:
+    sys.stderr.write(f"Usage: {sys.argv[0]} INPUT_FILE")
+
+INPUT_PATH = sys.argv[1]
+
+valid = 0
+with open(INPUT_PATH, 'r') as f:
+    for line in f:
+        if is_valid(line.strip()):
+            valid += 1
+
+print(valid)
