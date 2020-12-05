803852045407b02cc52d3966299d8d15ecaf0c75
Richard Belleville <j.belleville.richard@gmail.com>
2020-12-04 23:12:28 -0800

    Complete day 5, part 1

---
+++ 2020/5/5.py
@@ -0,0 +1,49 @@
+import argparse
+
+from typing import List
+
+
+def get_binary_coord(bsp_code: str, maximum: int, down_char: str,
+                     up_char: str) -> int:
+    low = 0
+    high = maximum - 1
+    for c in bsp_code:
+        if c == down_char:
+            high = (high + low) // 2
+        elif c == up_char:
+            low = -(-(high + low) // 2)
+        else:
+            assert False, "Encountered unknown character '{}' in '{}'".format(
+                c, bsp_code)
+    assert low == high, "{}, {}".format(low, high)
+    return low
+
+
+def get_y(bsp_code: str) -> int:
+    return get_binary_coord(bsp_code, 128, "F", "B")
+
+
+def get_x(bsp_code: str) -> int:
+    return get_binary_coord(bsp_code, 8, "L", "R")
+
+
+def get_seat_id(bsp_code: str) -> int:
+    y = get_y(bsp_code[:7])
+    x = get_x(bsp_code[-3:])
+    return y * 8 + x
+
+
+def get_seat_ids(path: str) -> List[int]:
+    seat_ids = []
+    with open(path, "r") as f:
+        for line in f:
+            seat_ids.append(get_seat_id(line.strip()))
+    return seat_ids
+
+
+parser = argparse.ArgumentParser(description="Solve day 5.")
+parser.add_argument("boarding_pass_file", help="The boarding pass file.")
+args = parser.parse_args()
+
+seat_ids = get_seat_ids(args.boarding_pass_file)
+print(max(seat_ids))
