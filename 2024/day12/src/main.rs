5310776dba06fbc81d88b8deb59c5c7b293a4afe
Richard Belleville <j.belleville.richard@gmail.com>
2024-12-28 16:07:42 -0800

    Solve day 12, part 2

--- 2024/day12/src/main.rs
+++ 2024/day12/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::vec::Vec;
 use std::collections::HashSet;
+use std::cmp;
 
 const DIRS: &[(i64, i64)] = &[
     (-1, 0),
@@ -82,9 +83,9 @@ impl<'a> Region<'a> {
         }
     }
 
-    pub fn region_kind(&self) -> char {
-        self.kind
-    }
+    // pub fn region_kind(&self) -> char {
+    //     self.kind
+    // }
 }
 
 struct Regions<'a> {
@@ -145,6 +146,82 @@ impl<'a> Regions<'a> {
     }
 }
 
+// TODO: Only build adjacency graphs once for the entire grid, rather than once per region.
+fn count_edges(region: &HashSet<(usize, usize)>) -> usize {
+    // x_max, y_max
+    let maxes: (usize, usize) = region.iter()
+                                        .fold((0, 0),
+                                              |acc, x| (cmp::max(acc.0, x.0), cmp::max(acc.1, x.1)));
+    let bounds = (maxes.0 + 1, maxes.1 + 1);
+
+    let mut adjacencies: Vec<Vec<Vec<bool>>> =
+        DIRS.iter().map( |_dirs|
+            (0..bounds.1).map( |_y|
+                (0..bounds.0).map( |_x| false).collect()
+            ).collect()
+        ).collect();
+
+    // Calculate adjacencies
+    for cell in region.iter() {
+        for (dir_index, dir) in DIRS.iter().enumerate() {
+            // TODO: Clean this up.
+            if let Some(adj) = adjacent(*cell, *dir, bounds.0, bounds.1) {
+                if !region.contains(&adj) {
+                    adjacencies[dir_index][cell.1][cell.0] = true;
+                }
+            } else {
+                adjacencies[dir_index][cell.1][cell.0] = true;
+            }
+        }
+    }
+
+
+    // for (dir_index, _dir) in DIRS.iter().enumerate() {
+    //     for row in adjacencies[dir_index].iter() {
+    //         for col in row.iter() {
+    //             if *col {
+    //                 print!("1 ");
+    //             } else {
+    //                 print!("0 ");
+    //             }
+    //         }
+    //         print!("\n");
+    //     }
+    //     print!("\n");
+    // }
+
+    let mut side_count = 0;
+    for (dir_index, _dir) in DIRS.iter().enumerate() {
+        let mut unvisited = region.clone();
+        while let Some(c) = unvisited.iter().next() {
+            // print!("  Pulling cell ({}, {})\n", c.0, c.1);
+            let mut side_length = 0;
+            let mut to_visit: Vec<(usize, usize)> = vec![*c];
+            while let Some(n) = to_visit.pop() {
+                // print!("  Inspecting cell ({}, {})\n", n.0, n.1);
+                unvisited.remove(&n);
+                if !adjacencies[dir_index][n.1][n.0] {
+                    continue;
+                }
+                side_length += 1;
+                for adj in adjacent_cells(n, bounds.0, bounds.1) {
+                    if unvisited.contains(&adj) {
+                        to_visit.push(adj);
+                    }
+                }
+            }
+
+            if side_length > 0 {
+                side_count += 1;
+                // print!("  Counted {side_count} sides\n");
+            }
+        }
+    }
+
+    // print!("Total {side_count} sides\n");
+    side_count
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -164,39 +241,21 @@ fn main() {
         .map(|x| x.chars().collect())
         .collect();
 
-    let grid_height = grid.len();
-    let grid_width = grid[0].len();
-
     let mut regions = Regions::new(&grid);
 
     let mut total_price: usize = 0;
 
-    // TODO: Figure out if this can be made better.
-    loop {
-        if let Some(mut region) = regions.next() {
-            let mut region_cells = HashSet::new();
-            let mut area = 0;
+    while let Some(mut region) = regions.next() {
+        let mut region_cells: HashSet<(usize, usize)> = HashSet::new();
+        let mut area = 0;
 
-            loop {
-                if let Some(cell) = region.next() {
-                    area += 1;
-                    region_cells.insert(cell);
-                } else {
-                    break;
-                }
-            }
-
-            let perimeter: usize = region_cells.iter()
-                                    .map(|cell| 4 - adjacent_cells(cell.clone(), grid_width, grid_height)
-                                                    .filter(|adj| region_cells.contains(adj))
-                                                    .count())
-                                    .sum();
-
-            total_price += area * perimeter;
-
-        } else {
-            break;
+        // Can this just be a collect method call followed by a len method call?
+        while let Some(cell) = region.next() {
+            area += 1;
+            region_cells.insert(cell);
         }
+
+        total_price += count_edges(&region_cells) * area;
     }
 
     println!("{total_price}");
